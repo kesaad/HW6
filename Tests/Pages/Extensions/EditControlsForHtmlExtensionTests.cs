68df9ddb38b4f3f171cb68b487b60db50e372602
kesaad <kesaad@ttu.ee>
2020-03-20 21:01:12 +0200

    P'rast 40.videot

--- Tests/Pages/Extensions/EditControlsForHtmlExtensionTests.cs
+++ Tests/Pages/Extensions/EditControlsForHtmlExtensionTests.cs
@@ -1,4 +1,7 @@
-﻿using Abc.Pages.Extensions;
+﻿using System.Collections.Generic;
+using Abc.Facade.Quantity;
+using Abc.Pages.Extensions;
+using Microsoft.AspNetCore.Html;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Abc.Tests.Pages.Extensions
@@ -11,7 +14,16 @@ namespace Abc.Tests.Pages.Extensions
         [TestMethod]
         public void EditControlsForTest()
         {
-            Assert.Inconclusive();
+            var obj = new htmlHelperMock<UnitView>().EditControlsFor(x => x.MeasureId);
+            Assert.IsInstanceOfType(obj, typeof(HtmlContentBuilder));
+        }
+
+        [TestMethod]
+        public void HtmlStringsTest()
+        {
+            var expected = new List<string> { "<div", "LabelFor", "EditorFor", "ValidationMessageFor", "</div>" };
+            var actual = EditControlsForHtmlExtension.htmlStrings(new htmlHelperMock<MeasureView>(), x => x.ValidFrom);
+            TestHtml.Strings(actual, expected);
         }
     }
 }
