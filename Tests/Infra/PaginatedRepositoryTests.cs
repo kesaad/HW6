452d337b14467c34749cba4cba91a61b10d575bc
kesaad <kesaad@ttu.ee>
2020-03-29 22:06:15 +0300

    P'rast 50.videot

---
+++ Tests/Infra/PaginatedRepositoryTests.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Abc.Aids;
+using Abc.Data.Quantity;
+using Abc.Domain.Quantity;
+using Abc.Infra;
+using Abc.Infra.Quantity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Abc.Tests.Infra
+{
+    [TestClass]
+    public class PaginatedRepositoryTests { }
+}
